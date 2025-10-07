6b26715df3145d8ac4f4537ea29859cc7c01171c
BHUMI LODAYA <bhumilodaya23@gmail.com>
2025-10-07 11:39:26 +0530

    Time: 321 ms (21.82%), Space: 66.8 MB (91.94%) - LeetHub

---
+++ 2891-method-chaining/2891-method-chaining.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    result_df= animals[animals['weight']>100].sort_values(
+        by='weight',
+        ascending=False
+    )[['name']]
+    return result_df
